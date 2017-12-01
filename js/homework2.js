0681f40381192624e9ed8640bc50f482eadd34d5
Reiby1982 <kushin@yandex.ru>
2017-12-01 19:41:04 +0300

    homework2

--- js/homework2.js
+++ js/homework2.js
@@ -26,15 +26,45 @@ if (varDetergent===0) {
 
 
 /*Задача 2 
-Создать таблицу соответствия между весом в фунтах и весом в килограммах для дначений от 1 до 10*/
+Создать таблицу соответствия между весом в фунтах и весом в килограммах для значений от 1 до 10*/
 console.log(" ");
 console.log("Задача 2");
+
+let varFt = 1;
+
+for (;varFt<11;varFt++) {
+	let varkG = varFt * 0.453592;
+	console.log(varFt, 'фт =' , varkG, 'кг');
+}
 
 /*Задание 3
 Посмотреть, как работают методы для работы с массивами: join, slice, indexOf/lastIndexOf, concat.
 Разобрать по 1му примену на каждый из них. В субботу обсудим*/
 console.log(" ");
 console.log("Задача 3");
+console.log(" ");
+console.log("метод join");
+var arrJoin = ['Маша', 'Петя', 'Марина', 'Василий'];
+var strJoin = arrJoin.join(', ');
+console.log(arrJoin);
+console.log(strJoin);
+
+console.log(" ");
+console.log("метод slice");
+var arrSlice = ['Один', 'Два', 'Три', 'Четыре', 'Пять', 'Шесть']
+var arrSlice2 = arrSlice.slice(0, 3); // элементы 0, 1, 2 (не включая 3)
+console.log('исходный массив', arrSlice);
+console.log('копируемый массив',arrSlice2);
+
+console.log(" ");
+console.log("метод indexOf/lastIndexOf");
+
+console.log(" ");
+console.log("метод concat");
+var arrConcat = [1, 2, 3];
+var arrConcat2 = arrConcat.concat('четыре', 'пять');
+console.log('исходный массив',arrConcat);
+console.log('новый массив',arrConcat2);
 
 /*Задача 4
 Есть три вершины. Координаты вершин (x1, x2), (y1, y2), (z1, z2) заданы как целые числа. Нужно определить - является ли треугольник с заданными координатами прямоугольным.
@@ -47,3 +77,7 @@ console.log("Задача 4");
 Создать массив из чисел. Выполнить сортировку массива по возрастанию методом пузырька. Запрещено использовать стандартные методы.*/
 console.log(" ");
 console.log("Задача 5");
+var arrBubble = [3, 4, 23, 5634, 32, 12, 10, 11, 7, 34];
+console.log("Заданный массив", arrBubble);
+let varN = arrBubble.length-1;
+
