b091ebf31f8bd435fbf598fabbe4372ffccb3fca
Reiby1982 <kushin@yandex.ru>
2017-12-14 19:09:33 +0300

    Lesson 8 Json Jquerry

---
+++ json_jquerry/js/jquerry.js
@@ -0,0 +1,81 @@
+/*
+В js файле создать объект handlers со следующими методами:
+startDraw
+endDraw
+drawing
+erase
+
+на canvas повесить следующие обработчики:
+'mousedown' - метод handlers - startDraw
+'mouseup'- метод handlers - endDraw
+'mouseout' - метод handlers - endDraw
+'mousemove'- метод handlers - drawing
+
+Проверьте, чтобы методы действительно отрабатывали. 
+Во вторник будем делать из этого мини paint. 
+Но можете попробовать реализовать его сами до вторника (надо будет почитать про рисование canvas)
+*/
+
+(function() {
+	'use strict';
+
+
+let canvas = document.getElementById("canvas");
+let erase = document.getElementById("paint_eraser");  
+
+let handlers = {
+	startDraw: function() {
+		console.log("начало рисования");
+		this.isDrawing = true;
+	},
+	endDraw: function() {
+		console.log("конец рисования");
+		this.isDrawing = false;
+	},
+	drawing: function(event) {
+		if (this.isDrawing) {
+			console.log("рисование в процессе");
+
+			var context = canvas.getContext('2d'); // задание координатной сетки x и y
+			context.beginPath(); // начать рисование
+            
+            //context.strokeStyle рисование контуром
+			// contex.fillStyle() рисование заливкой
+			context.fillStyle = document.getElementById('paint_brush_color').value;
+			context.arc(event.offsetX, event.offsetY, 
+					document.getElementById('paint_brush_size').value,
+					0, Math.PI*2, false);
+			context.fill();
+
+
+		} else {
+			return;
+		}
+	},
+	erase: function () {
+		console.log("стирание");
+        var context = canvas.getContext('2d');
+			context.beginPath();
+			context.fillStyle = document.getElementById('paint_brush_color').value =  '#ffffff';
+			context.arc(event.offsetX, event.offsetY, 
+					document.getElementById('paint_brush_size').value ,
+					0, Math.PI*2, false);
+			context.fill();
+        
+	},
+};
+
+
+	canvas.addEventListener('mousedown', handlers.startDraw);
+	canvas.addEventListener('mouseup', handlers.endDraw);
+	canvas.addEventListener('mouseout', handlers.endDraw);
+	canvas.addEventListener('mousemove', handlers.drawing);
+    erase.addEventListener('click', handlers.erase);
+
+	// // получить координаты мыши 
+	// elem.getBoundingClientRect() {
+	// 	"top" :;
+	// 	'bottom':;
+	// }
+
+}());
