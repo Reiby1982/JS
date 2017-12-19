954dbc6b6ce37c1c885cf3a8dc6d114243f61200
Reiby1982 <kushin@yandex.ru>
2017-12-19 21:31:58 +0300

    Lesson 10

---
+++ json_jquerry/js/Classes.js
@@ -0,0 +1,19 @@
+Game | Round 
+start
+round переключение между игроком и комп
+finRound - метод
+finish
+
+Player
+name
+simbol (x | o)
+status
+
+
+GameTable // контроллер
+getUserInfo - собрет информацию
+updateField
+checkField
+
+
+
